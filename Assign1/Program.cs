2bc660fd19d9ca962c002a50088d02d7526916ae
=riteshvadnere <=>
2020-12-03 20:36:55 +0530

    Updated

---
+++ Assign1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
+using System.Windows;
+
+namespace Assign1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a no.");
+            var num = Convert.ToByte(Console.ReadLine());
+            for (int i = num; i >= 1; i--)
+            {
+                for (int j = 1; j <=i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
